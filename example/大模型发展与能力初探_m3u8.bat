2366c7a900926773e1e9de5d31c79ef96498df75
Jing Li <abc1870819884@gmail.com>
2024-03-19 09:44:43 +0800

    first commit

---
+++ example/大模型发展与能力初探_m3u8.bat
@@ -0,0 +1,3 @@
+@echo off
+CHCP 65001
+"N_m3u8DL-CLI_v3.0.2.exe" "https://encrypt-k-vod.xet.tech/2919df88vodtranscq1252524126/b912a67c3270835015222810949/drm/v.f421220.m3u8?sign=3814a4a4b69154076e0ad2ad7d5d5673&t=65f91424&us=tTtEPLhwGR" --enableDelAfterDone --workDir "C:\小鹅通店铺资源下载\applbv2gfct5909\大模型发展与能力初探" --saveName "大模型发展与能力初探" --maxThreads "200" --minThreads "20" 
